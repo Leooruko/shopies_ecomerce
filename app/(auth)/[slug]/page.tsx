701c401f6c167d68c9b5919d37d18b3cce4ffec4
leonoruko <leon.oruko@students.jkuat.ac.ke>
2025-02-02 23:27:08 +0300

    app user context

---
+++ app/(auth)/[slug]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import ProductSingleCard from "@/components/product_singleCard";
+import { useAppContext } from "@/hooks/useAppContext";
+import { useProduct } from "@/hooks/useStore";
+import { useParams } from "next/navigation";
+export default function ProductDetailsPage({}) {
+  const params = useParams();
+  const { slug } = params;
+  const { user } = useAppContext();
+  const { data: product, isLoading, error } = useProduct(user.id, slug);
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+  return (
+    <>
+      <ProductSingleCard product={product} />
+    </>
+  );
+}
